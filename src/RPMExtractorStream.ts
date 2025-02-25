55a796f05514eaf3e3b7628981221e9b35e70f7b
Brendan Early <mymindstorm@evermiss.net>
2025-02-24 23:50:00 -0600

    i tried but the cpio lib is too buggy. or my code is just garabge

--- src/RPMExtractorStream.ts
+++ src/RPMExtractorStream.ts
@@ -5,8 +5,8 @@ import isXzRpm from "./isXzRpm";
 import zlib from "zlib";
 import lzma from "lzma-native";
 // @ts-expect-error no types and I don't feel like wrangling esbuild
-import stream from "cpio-stream";
-import concat from "concat-stream";
+import cpioStream from "cpio-stream";
+import { finished, pipeline, Stream, Writable } from "node:stream";
 
 export interface ExtractedFile {
   stream: PassThrough;
@@ -26,25 +26,30 @@ interface CpioHeader {
   linkname?: string;
 }
 
+let myBuf = new Buffer(0);
+
 export class RPMExtractorStream extends Transform {
   private headerValidated = false;
   private buffer = Buffer.alloc(0);
-  private decompressor?: Transform;
+  private rpmDecompressStream?: Transform;
   private decompressorOffset?: number;
-  private decompressStream;
+  private cpioDecompressStream: PassThrough;
 
   constructor() {
     super({ objectMode: true });
 
-    this.decompressStream = stream.extract();
-    this.decompressStream.on("entry", this.handleCpioEntry.bind(this));
+    this.cpioDecompressStream = cpioStream.extract();
+    this.cpioDecompressStream.on("entry", this.handleCpioEntry.bind(this));
+    this.cpioDecompressStream.on("error", (err: Error) =>
+      this.emit("error", err),
+    );
   }
 
-  _transform(
+  async _transform(
     chunk: Buffer,
     _encoding: BufferEncoding,
     callback: TransformCallback,
-  ): void {
+  ): Promise<void> {
     // Check if this is a valid RPM
     if (!this.headerValidated) {
       // QUESTIONABLE: the first chunk _has_ to be more than 4 bytes, right?
@@ -57,32 +62,47 @@ export class RPMExtractorStream extends Transform {
     }
 
     // Decompress the RPM
-    if (!this.decompressor) {
+    if (!this.rpmDecompressStream) {
       // This could be kept to 20 bytes-ish window, but who cares?
       this.buffer = Buffer.concat([this.buffer, chunk]);
 
       if ((this.decompressorOffset = isGzRpm(this.buffer)) !== -1) {
-        this.decompressor = zlib.createUnzip();
+        this.rpmDecompressStream = zlib.createUnzip();
       } else if ((this.decompressorOffset = isXzRpm(this.buffer)) !== -1) {
-        this.decompressor = lzma.createDecompressor();
+        this.rpmDecompressStream = lzma.createDecompressor();
       }
 
-      if (!this.decompressor) return callback();
+      if (!this.rpmDecompressStream) return callback();
+      this.rpmDecompressStream.on("error", (err: Error) =>
+        this.emit("error", err),
+      );
 
-      this.decompressor.write(this.buffer.subarray(this.decompressorOffset));
+      // this.rpmDecompressStream.pipe(this.cpioDecompressStream);
+      this.rpmDecompressStream.write(
+        this.buffer.subarray(this.decompressorOffset),
+      );
+      myBuf = this.buffer.subarray(this.decompressorOffset);
       this.buffer = Buffer.alloc(0); // Not sure if this helps gc get rid of the buffer
     } else {
-      this.decompressor.write(chunk);
+      this.rpmDecompressStream.write(chunk);
+      myBuf = Buffer.concat([myBuf, chunk]);
     }
 
     return callback();
   }
 
-  private handleCpioEntry(
-    header: CpioHeader,
-    stream: PassThrough,
-    callback: () => void,
-  ) {
+  _flush(callback: TransformCallback): void {
+    if (this.rpmDecompressStream) {
+      this.rpmDecompressStream.end();
+      this.cpioDecompressStream.write(zlib.unzipSync(myBuf));
+      finished(this.cpioDecompressStream, (err) => callback());
+    } else {
+      callback();
+    }
+  }
+
+  private handleCpioEntry(header: CpioHeader, stream: PassThrough) {
+    console.log("emitting file ", header.name);
     this.emitFile({
       stream: stream,
       mode: header.mode,
